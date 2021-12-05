96e4309b9e61aa0f45fa0a7b52e83d5668df1a1b
MOHAMMEDZAMEER <zameer4939@gmail.com>
2021-12-05 00:47:41 -0400

    PassWord contains At Least One UpperCase

--- src/com/regex/UserTest.java
+++ src/com/regex/UserTest.java
@@ -7,7 +7,7 @@ public class UserTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner z = new Scanner(System.in);
-		System.out.println("Enter User PassWord");
+		System.out.println("Enter User PassWord Should Have At Least One UpperCase");
 
 		String  name = z.nextLine();
 		
