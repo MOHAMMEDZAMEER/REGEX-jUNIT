5f242afcf9e517eaf5eba2fd5c6327c84e88a773
MOHAMMEDZAMEER <zameer4939@gmail.com>
2021-12-05 03:49:27 -0400

    Refactor Email code by Usecase9 in pdf

--- src/com/regex/UserTest.java
+++ src/com/regex/UserTest.java
@@ -7,12 +7,12 @@ public class UserTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner z = new Scanner(System.in);
-		System.out.println("Enter User PassWord Should Have One Special Charector");
+		System.out.println("Enter Email Samples");
 
 		String  name = z.nextLine();
 		
 		User user = new User();
-		user.Password(name);
+		user.Email(name);
 			
 			z.close();
 		
